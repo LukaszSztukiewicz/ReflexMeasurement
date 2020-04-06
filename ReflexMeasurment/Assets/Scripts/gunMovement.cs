a7fe83b6ca56c1356d7d77a65552b028d25cd93b
Mikolaj-Szawerda23 <mikolaj.szawerda@wp.pl>
2020-04-06 15:21:41 +0200

    Ruch bronii
    -broń śledzi ruchy myszy

---
+++ ReflexMeasurment/Assets/Scripts/gunMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunMovement : MonoBehaviour
+{
+    public Camera mainCamera;
+    
+    void Start()
+    {
+    }
+    void Update()
+    {
+
+        Move();
+
+    }
+    void Move()
+    {
+        float offsetX = 90f, offsetZ = 90f;
+        Vector3 mousePosition = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, mainCamera.transform.position.z * -1));
+        float Z = mousePosition.z - transform.position.z;
+        transform.rotation = Quaternion.Euler(Mathf.Abs(offsetX - Mathf.Atan2(mousePosition.y - transform.position.y, Z) * Mathf.Rad2Deg), 0f, Mathf.Abs(offsetZ - Mathf.Atan2(mousePosition.x - transform.position.x, Z) * Mathf.Rad2Deg));
+    }
+}
