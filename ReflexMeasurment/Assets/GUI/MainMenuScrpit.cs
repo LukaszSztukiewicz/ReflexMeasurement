cc4b33f7660c8bcf1ef01ea46a06094df561bd92
lukasz sz <sztukiewicz.lukasz@gmail.com>
2020-04-06 15:39:08 +0200

    MainMenu

---
+++ ReflexMeasurment/Assets/GUI/MainMenuScrpit.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MainMenuScrpit : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject MoreMenu;
+    public GameObject MoreButton;
+    public GameObject StartButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MoreMenu.SetActive(false);
+        MainMenu.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Play()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(1);
+    }
+    public void More()
+    {
+        MainMenu.SetActive(false);
+        MoreMenu.SetActive(true);
+    }
+    public void Back()
+    {
+        MoreMenu.SetActive(false);
+        MainMenu.SetActive(true);
+       
+    }
+}
